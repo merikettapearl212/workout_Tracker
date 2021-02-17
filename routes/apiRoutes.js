7b6970a4d88a54735fdb13eeb09baccfe1d7b552
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2021-02-16 20:50:44 -0800

    working on connecting to mongoose

---
+++ routes/apiRoutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+// const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+
+});
+
+router.post("/api/workouts", (req, res) => {
+
+});
+
+router.get("/api/workouts/range", (req, res) => {
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,34 @@
+// Require the following npm packages inside of the server.js file:
 const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3060;
+// Load models file into mongoose
+require("./models");
 
+const PORT = process.env.PORT || 3030;
+
+// Create express server
 const app = express();
+// Tell our server to use logger/morgan
+app.use(logger("dev"));
+// Parse application body as JSON/Process dynamic Data with middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+
+// Connection to mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true }, { useUnifiedTopology: true });
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Import routes and give the server access to them.
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, () => {
